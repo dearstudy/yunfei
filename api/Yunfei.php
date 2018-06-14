f576d689f0d2d522aa9e3410c0c521bb3c2d2ab6
vini123 <lichking_lin86@qq.com>
2018-06-14 23:14:39 +0800

    controller， model的编写

---
+++ api/Yunfei.php
@@ -0,0 +1,151 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Yunfei extends MY_Controller {
+
+	public function index()
+	{
+
+	}
+
+	// 注册
+	public function register()
+	{
+		if (FALSE === $this->input->is_ajax_request())
+		{
+			$this->output->json(array('code' => 10000, 'error' => '不正确请求'));
+		}
+
+		$filename = $this->input->post('filename');
+		if(FALSE === isset($filename))
+		{
+			$this->output->json(array('code' => 10000, 'error' => '缺失参数[0]'));
+		}
+
+		$myfile = $this->input->post('myfile');
+		if(FALSE === isset($myfile))
+		{
+			$this->output->json(array('code' => 10000, 'error' => '缺失参数[1]'));
+		}
+
+		$name = $this->input->post('name');
+		if(FALSE === isset($name))
+		{
+			$this->output->json(array('code' => 10000, 'error' => '缺失参数[2]'));
+		}
+
+		$phone = $this->input->post('phone');
+		if(FALSE === isset($phone))
+		{
+			$this->output->json(array('code' => 10000, 'error' => '缺失参数[3]'));
+		}
+
+		$card = $this->input->post('card');
+		if(FALSE === isset($card))
+		{
+			$this->output->json(array('code' => 10000, 'error' => '缺失参数[4]'));
+		}
+
+		$sex = $this->input->post('sex');
+		if(FALSE === isset($sex))
+		{
+			$this->output->json(array('code' => 10000, 'error' => '缺失参数[5]'));
+		}
+
+		$borthday = $this->input->post('borthday');
+		if(FALSE === isset($borthday))
+		{
+			$this->output->json(array('code' => 10000, 'error' => '缺失参数[6]'));
+		}
+
+		$weight = $this->input->post('weight');
+		if(FALSE === isset($weight))
+		{
+			$this->output->json(array('code' => 10000, 'error' => '缺失参数[7]'));
+		}
+
+		$path =  FCPATH . "static/yunfei/avatar/";
+		$result = $this->uploadBase64($myfile, $path, $filename);
+		if(TRUE == $result)
+		{
+			$filename = base_url() . "static/yunfei/avatar/" . $filename;
+			$this->load->model('yunfei/Yunfei_model');
+			$data = array('names' => $name,
+														'phone' => $phone,
+														'cards' => $card,
+														'sex' => $sex,
+														'borthdays' => $borthday,
+														'weight' => $weight,
+														'image_name' => $filename);
+
+			$result = $this->Yunfei_model->register($data);
+			$this->output->json($result);
+		}
+		$this->output->json(array('code' => 10000, 'error' => '上传头像错误[8]'));
+	}
+
+	// 注册
+	public function signup()
+	{
+		if (FALSE === $this->input->is_ajax_request())
+		{
+			$this->output->json(array('code' => 10000, 'error' => '不正确请求'));
+		}
+
+		$phone = $this->input->post('phone');
+		if(FALSE === isset($phone))
+		{
+			$this->output->json(array('code' => 10000, 'error' => '缺失参数[0]'));
+		}
+
+		$changg = $this->input->post('changg');
+		if(FALSE === isset($changg))
+		{
+			$this->output->json(array('code' => 10000, 'error' => '缺失参数[1]'));
+		}
+
+		$this->load->model('yunfei/Yunfei_model');
+		$data = array('localtimes' => $changg,
+													'phone' => $phone);
+
+		$result = $this->Yunfei_model->signup($data);
+		$this->output->json($result);
+	}
+
+	public function ranking()
+	{
+		if (FALSE === $this->input->is_ajax_request())
+		{
+			$this->output->json(array('code' => 10000, 'error' => '不正确请求'));
+		}
+
+		$this->load->model('yunfei/Yunfei_model');
+		$result = $this->Yunfei_model->ranking();
+		$this->output->json($result);
+	}
+
+	private function uploadBase64($basestr, $path, $name)
+	{
+			if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $basestr, $result))
+			{
+					$type = $result[2];
+
+					if (!file_exists($path))
+					{
+							mkdir($path, 0777, TRUE);
+					}
+
+					//写入操作
+					if (file_put_contents($path.$name, base64_decode(str_replace($result[1], '', $basestr))))
+					{
+							return true;
+					}
+					else
+					{
+							return false;
+					}
+			}
+			return false;
+	}
+
+}
---
+++ api/Yunfei_model.php
@@ -0,0 +1,81 @@
+<?php
+
+class Yunfei_model extends CI_Model
+{
+	private $db = NULL;
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->db = $this->load->database('yunfei', TRUE);
+	}
+
+	public function __destruct()
+	{
+		if (isset($this->db))
+		{
+			$this->db->close();
+			$this->db = NULL;
+		}
+	}
+
+	public function register($value)
+	{
+			$phone = $value['phone'];
+			if(FALSE === isset($phone))
+				return array('code'=>10000 , 'error'=>'缺少电话号码');
+
+			$borthdays = $value['borthdays'];
+			if(FALSE === isset($borthdays))
+				return array('code'=>10000 , 'error'=>'缺少身份证号码');
+
+			$query = $this->db->get_where('user', array('phone' => $phone));
+			if ($query->num_rows() > 0)
+			{
+				return array('code'=>10000 , 'error'=>'该电话号码已经报名了');
+			}
+
+			$query = $this->db->get_where('user', array('borthdays' => $borthdays));
+			if ($query->num_rows() > 0)
+			{
+				return array('code'=>10000 , 'error'=>'该身份证号码已经报名了');
+			}
+
+			$query = $this->db->insert('user', $value);
+			if ($query == FALSE)
+			{
+				return array('code' => 10000, 'error' => '注册失败');
+			}
+			return array('code' => 0, 'error' => '注册成功');
+	}
+
+	public function signup($value)
+	{
+			$phone = $value['phone'];
+			if(FALSE === isset($phone))
+				return array('code'=>10000 , 'error'=>'缺少电话号码');
+
+			$localtimes = $value['localtimes'];
+			if(FALSE === isset($localtimes))
+				return array('code'=>10000 , 'error'=>'缺少场馆');
+
+			$query = $this->db->where('phone', $phone)->update('user', array('localtimes' => $localtimes));
+			if(FALSE ==$query)
+			{
+				return array('code' => 10000, 'error' => '报名失败');
+			}
+			return array('code' => 0, 'error' => '报名成功');
+	}
+
+	public function ranking()
+	{
+		$query = $this->db->select('*')->from('user')->order_by('counts', 'DESC')->get();
+		if(FALSE == $query)
+		{
+			return array('code' => 10000, 'error' => '查询失败');
+		}
+		$data = $query->result_array();
+		return array('code' => 0, 'attr' => $data);
+	}
+}
